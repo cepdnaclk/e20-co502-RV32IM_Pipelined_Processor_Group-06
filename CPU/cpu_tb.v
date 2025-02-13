1935fb6f2433884adeca91ff2bf3326a1113ba38
RaviAlpha <ravindupathirage12345@gmail.com>
2025-02-13 16:25:10 +0530

    basic

--- CPU/cpu_tb.v
+++ CPU/cpu_tb.v
@@ -22,7 +22,7 @@ module cpu_tb;
 
         // Add stimulus here
         // Test case 1: Normal operation
-        #600;
+        #800;
 
         // Test case 2: Reset during operation
         RST = 1;
